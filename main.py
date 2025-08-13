2405ddadb45d4988bbc894a94241968c8944b3cc
Remixing <xxxxxremixingxxxxx@gmail.com>
2025-08-13 18:56:30 +0700

    Update: comment test agent

--- main.py
+++ main.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     from uagents import Bureau
     bureau = Bureau()
     bureau.add(moderator_agent)
-    bureau.add(test_client_agent)
+    # bureau.add(test_client_agent)
     bureau.run()
